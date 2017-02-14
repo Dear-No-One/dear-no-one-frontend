f07182a4e190eeb36d9c1821aac392f8d075e893
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-02-14 08:58:20 -0700

    got config api working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
-import CONFIG from '../config/environment';
+    import CONFIG from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  // namespace: 'api',
-  // host: CONFIG.apiEndpoint
-  host: 'http://localhost:3000'
+  namespace: 'api',
+  host: CONFIG.APP.apiEndpoint
+  // host: 'http://localhost:3000'
 });
