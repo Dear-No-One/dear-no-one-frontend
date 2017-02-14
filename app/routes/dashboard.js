c37ead767001d5d714345d96e0b4d1fceeff6479
ZacJocelyn <zacjocelyn@gmail.com>
2017-02-14 12:33:37 -0700

    we logged in

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,24 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-      return [{
-          title: 'Emo Bunny',
-          body: 'I cut my little bunny paws with razor blades',
-          post_url: '?????',
-          date_posted: '?',
-
-      }, {
-          title: 'Coding for dorks',
-          body: 'come learn to code with all your dork friends',
-          post_url: '?????',
-          date_posted: '?',
-
-      }, {
-          title: 'Money',
-          body: 'I need money',
-          post_url: '?????',
-          date_posted: '?',
-        
-    }];
+      return this.get('store').findAll('blog');
   }
 });
