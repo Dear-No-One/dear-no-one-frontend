0f6c40de5a3951f8c6555e28b3236dfc3d52c920
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-02-13 21:13:19 -0700

    :sparkles: get user route working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import CONFIG from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api',
-  host: CONFIG.apiEndpoint
+  // namespace: 'api',
+  // host: CONFIG.apiEndpoint
+  host: 'http://localhost:3000'
 });
--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,9 +1,4 @@
+import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    payload = {
-      cats: payload.data
-    };
 
-    return this._super(store, primaryModelClass, payload, id, requestType);
-  }
 });
