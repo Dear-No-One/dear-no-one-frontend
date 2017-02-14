6fc73f92357dda8498555104a5e0e5678979ac53
ZacJocelyn <zacjocelyn@gmail.com>
2017-02-14 13:34:41 -0700

    we can login boi

--- app/services/auth-manager.js
+++ app/services/auth-manager.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   accessToken: null,
 
-authenticate(login, password) {
+authenticate(email, password) {
   return Ember.$.ajax({
     method: "POST",
     url: "http://localhost:3000/api/token",
-    data: { email: login, password: password }
+    data: { email: email, password: password }
   }).then((result) => {
     console.log(result)
     this.set('accessToken', result.access_token);
