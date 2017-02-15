cc03b7d22b3e039e0dd9ae79f7d7401543aeb7ee
matt-winzer <matt.winzer@gmail.com>
2017-02-15 14:45:17 -0700

    create blog post form in dialog, logging post title and body

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -10,7 +10,7 @@ export default Ember.Controller.extend({
         this.set('showAnimatedDialog', false);
       },
       logForm() {
-        console.log(this.postBody);
+        console.log(this.postTitle, this.postBody);
       }
   }
 });
