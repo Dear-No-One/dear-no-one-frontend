2cd32f44a6485badeb52ec7439a42a4673d72c2e
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-02-16 14:51:18 -0700

    pulling component 2

--- app/components/blog-post-template-1.js
+++ app/components/blog-post-template-1.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isTrue: false,
+    actions: {
+  switchTheme() {
+      console.log('anything');
+    this.setProperties('isTrue');
+    console.log(this);
+  }
+}
 });
